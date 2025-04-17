141827596d9058e302fc072979459c7fe18b9815
Henok B Ademtew <46082799+HenokB@users.noreply.github.com>
2025-04-17 23:25:57 +0300

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,10 +2,10 @@ export const projects = [
   {
     id: 1,
     title: "HornMT",
-    languages_used: ["Python", "Pytorch"], // Languages and libraries used
-    creator: ["Lesan AI", "Asmelash Teka", "Gebrekirstos G. Gebremeskel"], // Who created the project
+    languages_used: ["Python", "Pytorch"], 
+    creator: ["Lesan AI", "Asmelash Teka", "Gebrekirstos G. Gebremeskel"], 
     description: `The HornMT repository contains data and the associated metadata for the project Machine Translation Benchmark Dataset for Languages in the Horn of Africa. It is a multi-way parallel corpus that will serve as a benchmark to accelerate progress in machine translation research and production systems for languages in the Horn of Africa.`,
-    featured: true, // true/false
+    featured: true, 
     domain: [
       "Health",
       "Politics",
@@ -15,8 +15,8 @@ export const projects = [
       "Art and Culture",
       "Disaster and Accidents",
     ],
-    category: ["Dataset"], // Dataset, Model, Paper
-    field: "NLP", // Computer Vision, NLP ...
+    category: ["Dataset"], 
+    field: "NLP", 
     links: [
       {
         id: 1,
@@ -29,20 +29,20 @@ export const projects = [
         link: "https://data.niaid.nih.gov/resources?id=zenodo_6369441",
       },
     ],
-    year: "2024", // The year that the project published
+    year: "2024", 
     content: "Dataset",
   },
 
   {
     id: 2,
     title: "Afro Chest X-ray",
-    languages_used: ["Python", "pydicom", "dicom2jpg"], // Languages and libraries used
-    creator: ["Negasi Haile", "Asmelash Teka", "Lesan AI"], // Who created the project
+    languages_used: ["Python", "pydicom", "dicom2jpg"], 
+    creator: ["Negasi Haile", "Asmelash Teka", "Lesan AI"], 
     description: `The Chest X-ray Imaging Dataset for Multiple Cardio-respiratory Diseases in Ethiopia (Afro Chest X-ray for short) is a project funded by the LacunaFund whose aim is to close the gap in health disparities by fostering interdisciplinary collaborations that create, expand, or aggregate labeled training and evaluation datasets.`,
-    featured: true, // true/false
-    domain: ["Health"], // Health, Language, Sport, Bussines, Agriculture
-    category: ["Dataset"], // Dataset, Model, Paper
-    field: "Computer Vision", // Computer Vision, NLP ...
+    featured: true, 
+    domain: ["Health"], 
+    category: ["Dataset"], 
+    field: "Computer Vision", 
     links: [
       {
         id: 1,
@@ -55,16 +55,16 @@ export const projects = [
         link: "https://github.com/asmelashteka/Afro-Chest-X-ray",
       },
     ],
-    year: "2024", // The year that the project published
+    year: "2024", 
     content: "Dataset",
   },
   {
     id: 3,
     title: "AGE: Amharic, English, Ge'ez Dataset",
-    languages_used: ["geez", "machine translation", "nlp"], // Languages and libraries used
-    creator: ["Henok Biadglign Ademtew", "Mikiyas Girma Birbo"], // Who created the project
+    languages_used: ["geez", "machine translation", "nlp"], 
+    creator: ["Henok Biadglign Ademtew", "Mikiyas Girma Birbo"], 
     description: `AGE, an open-source tripartite alignment of Amharic, Ge’ez, and English parallel dataset. Additionally, we introduced a novel, 1,000 Ge’ez-centered sentences sourced from areas such as news and novels.`,
-    featured: true, // true/false
+    featured: true, 
     domain: [
       "Religion",
       "Health",
@@ -74,8 +74,8 @@ export const projects = [
       "Bussiness",
       "Art and Culture",
     ],
-    category: ["Dataset"], // Dataset, Model, Paper
-    field: "NLP", // Computer Vision, NLP ...
+    category: ["Dataset"], 
+    field: "NLP", 
     links: [
       {
         id: 1,
@@ -88,22 +88,22 @@ export const projects = [
         link: "https://huggingface.co/datasets/Henok/age_dataset",
       },
     ],
-    year: "2024", // The year that the project published
+    year: "2024", 
     content: "Dataset",
   },
 
   {
     id: 4,
     title: "Amharic Llama",
-    languages_used: ["LLM", "Llama"], // Languages and libraries used
-    creator: ["Michael Andersland"], // Who created the project
+    languages_used: ["LLM", "Llama"], 
+    creator: ["Michael Andersland"], 
     description: `Uses Llama-2-7b as base. Does not use chat variant.`,
     featured: true, 
     domain: [
       "LLM",
     ],
-    category: ["Model"], // Dataset, Model, Paper
-    field: "NLP", // Computer Vision, NLP ...
+    category: ["Model"], 
+    field: "NLP", 
     links: [
       {
         id: 1,
@@ -121,7 +121,7 @@ export const projects = [
         link: "https://arxiv.org/abs/2403.06354",
       },
     ],
-    year: "2024", // The year that the project published
+    year: "2024", 
     content: "Model",
   },
 
